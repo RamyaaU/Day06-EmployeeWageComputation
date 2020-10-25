10e14e5316046500c1b38b7ab135b3ca4a2a5284
Ramya.U <ramyayadav2016@gmail.com>
2020-10-25 18:04:59 +0530

    Add project files.

---
+++ Day06-EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day06_EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage computation!");
+        }
+    }
+}
