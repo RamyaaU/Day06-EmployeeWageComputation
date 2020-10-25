72a163c4c2df973896dc0b01b5dda5a5208be773
Ramya.U <ramyayadav2016@gmail.com>
2020-10-25 18:07:23 +0530

    Added UC-1

--- Day06-EmployeeWageComputation/Program.cs
+++ Day06-EmployeeWageComputation/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿/******************************************************************************
+ *  Compilation:  csc.exe /t:exe /out:Day04EmployeeWageCompuatation.exe Day04EmployeeWageCompuatation.cs
+ *  Execution:    Day04EmployeeWageCompuatation.exe 103
+ *  
+ *  Purpose: Determines whether employee is present or not
+ *
+ *  @author  Ramya U
+ *  @version 1.0
+ *  @since   22-10-2020
+ *
+ ******************************************************************************/
+using System;
 
-namespace Day06_EmployeeWageComputation
+namespace Day04EmployeeWageComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage computationn!");
+            Console.WriteLine("Welcome to EmployeeWageComputation!");
+            //constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
